aa54d33de46fb17af2b48129cadc9acd3ec85705
Nartyna <mplutwska@gmail.com>
2022-10-25 21:45:31 +0200

    add timer to countdown to 28 november (announcement of the results)

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ setInterval(() => {
 	document.querySelector('.seconds').innerText = secs;
    if(diff==0) clearInterval();
 }, 1000);
+
+setInterval(() => {
+	const end = Date.parse('Nov 28, 2022 23:59:59');
+	const today = Date.parse(new Date());
+
+	const diff = end - today;
+
+	const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+	const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+	const mins = Math.floor(diff / 1000 / 60) % 60;
+	const secs = Math.floor(diff / 1000) % 60;
+
+	document.querySelector('.timer2').innerText = `${days}d ${hours}h ${mins}m ${secs}s`;
+}, 1000);
